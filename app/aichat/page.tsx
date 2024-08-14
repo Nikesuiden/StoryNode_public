96ed6bd2884164faaf0c3b998a29c93616a39f04
Nikesuiden <taiki.nikewp@gmail.com>
2024-08-14 18:08:10 +0900

    start project

---
+++ app/aichat/page.tsx
@@ -0,0 +1,3 @@
+export default function Aichat() {
+  return <div>ひかきんまにあ</div>;
+}
---
+++ app/start/page.tsx
@@ -0,0 +1,7 @@
+export default function Start() {
+  return (
+    <div>
+      <h2>StoryNode</h2>
+    </div>
+  );
+}
