23498d12e9a856cc8d1b5989bf03476dbed5450c
Nikesuiden <taiki.nikewp@gmail.com>
2024-08-30 14:51:43 +0900

    感情選択肢を設定、基本的感情に基づく

--- components/elements/diaryInput/diaryInput.tsx
+++ components/elements/diaryInput/diaryInput.tsx
@@ -52,8 +52,8 @@ export default function DiaryInput() {
           <InputLabel
             style={{
               backgroundColor: "white",
-              paddingLeft: '5px',
-              paddingRight: "5px"
+              paddingLeft: "5px",
+              paddingRight: "5px",
             }}
           >
             Emotion
@@ -64,9 +64,15 @@ export default function DiaryInput() {
             placeholder="Emotion"
             onChange={inputEmotionChange}
           >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
+            <MenuItem value={"none"}>--未設定--</MenuItem>
+            <MenuItem value={"grad"}>嬉しい</MenuItem>
+            <MenuItem value={"Funny"}>楽しみ</MenuItem>
+            <MenuItem value={"expectations"}>期待</MenuItem>
+            <MenuItem value={"happy"}>幸せ</MenuItem>
+            <MenuItem value={"surprise"}>驚き</MenuItem>
+            <MenuItem value={"sad"}>悲しい</MenuItem>
+            <MenuItem value={"angry"}>怒り</MenuItem>
+            <MenuItem value={"anxiety"}>不安</MenuItem>
           </Select>
         </FormControl>
         <Typography
--- components/elements/diaryList/diaryList.tsx
+++ components/elements/diaryList/diaryList.tsx
@@ -8,11 +8,12 @@ import {
   Select,
   TextField,
   Typography,
+  MenuItem,
 } from "@mui/material";
 
 export default function DiaryList() {
   return (
-    <Box sx={{ mt: 1.5,  }}>
+    <Box sx={{ mt: 1.5 }}>
       <FormControl
         sx={{
           minWidth: 120,
@@ -20,9 +21,17 @@ export default function DiaryList() {
         fullWidth
       >
         {/* <InputLabel>Emotion</InputLabel> */}
-        <Select
-          sx={{ width: "30%" , height: 40}}
-        ></Select>
+        <Select sx={{ width: "30%", height: 40 }}>
+          <MenuItem value={"none"}>--未設定--</MenuItem>
+          <MenuItem value={"grad"}>嬉しい</MenuItem>
+          <MenuItem value={"Funny"}>楽しみ</MenuItem>
+          <MenuItem value={"expectations"}>期待</MenuItem>
+          <MenuItem value={"happy"}>幸せ</MenuItem>
+          <MenuItem value={"surprise"}>驚き</MenuItem>
+          <MenuItem value={"sad"}>悲しい</MenuItem>
+          <MenuItem value={"angry"}>怒り</MenuItem>
+          <MenuItem value={"anxiety"}>不安</MenuItem>
+        </Select>
       </FormControl>
     </Box>
   );
