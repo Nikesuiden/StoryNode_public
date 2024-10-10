9e38ac34b6e453c8ccfaed5c090641d88a5a4bb9
Nikesuiden <taiki.nikewp@gmail.com>
2024-10-10 22:08:54 +0900

    ばrぎおあrん

--- components/elements/todoInput/todoInput.tsx
+++ components/elements/todoInput/todoInput.tsx
@@ -35,9 +35,9 @@ const ToDoInput: React.FC<ToDoListProps> = ({ onAction }) => {
 
     if (!session?.access_token) {
       return;
+    } else {
+      console.log("認証は通っている");
     }
-
-    console.log("認証は通っている")
 
     if (!todoInput.trim()) return;
 
@@ -52,7 +52,7 @@ const ToDoInput: React.FC<ToDoListProps> = ({ onAction }) => {
     if (response.ok) {
       setTodoInput("");
       await onAction();
-      alert("保存に成功しました。")
+      alert("保存に成功しました。");
     } else {
       alert("エラーが発生しました。もう一度お試しください。");
     }
