f41a714bd08c556f427957c7555d1bb38fb35fab
Nikesuiden <taiki.nikewp@gmail.com>
2024-10-22 08:28:00 +0900

    APIルートを構築

---
+++ app/api/OpenAI_tts/route.ts
@@ -0,0 +1,36 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import fs from "fs";
+import path from "path";
+import { openai } from "@/lib/openai";
+
+export default async function OpenAI_TTS(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const input = req.body.input;
+
+  if (!input) {
+    return res.status(407).json({ message: "Invalid input" });
+  }
+  // POST以外のリクエストを遮断
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method is not allowed" });
+  }
+
+  try {
+    const SpeechFile = path.resolve("./public/audio/speech.mp3");
+
+    // openaiにリクエスト
+    const mp3 = await openai.audio.speech.create({
+      model: "tts-1",
+      voice: "alloy",
+      input: input,
+    });
+
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    res.send(buffer);
+  } catch (error) {
+    console.error("Error generating speech:", error);
+    res.status(500).json({ message: "Error generating speech" });
+  }
+}
