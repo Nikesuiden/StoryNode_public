b62004335d2fd6dd70fead1752a1aab3efcee366
Nikesuiden <taiki.nikewp@gmail.com>
2024-10-24 14:16:54 +0900

    エラー修正2

--- app/api/OpenAI_tts/route.ts
+++ app/api/OpenAI_tts/route.ts
@@ -1,4 +1,4 @@
-import { NextApiResponse, NextApiRequest } from "next";
+import { NextResponse } from 'next/server';
 import OpenAI from "openai";
 import path from "path";
 
@@ -6,24 +6,17 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY_SELF || "",
 });
 
-export async function OpenAI_TTS(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const input = req.body.input;
+export async function POST(req: Request) {
+  const { input } = await req.json();
 
   if (!input) {
-    return res.status(407).json({ message: "Invalid input" });
-  }
-  // POST以外のリクエストを遮断
-  if (req.method !== "POST") {
-    return res.status(405).json({ message: "Method is not allowed" });
+    return NextResponse.json({ message: "Invalid input" }, { status: 407 });
   }
 
   try {
     const SpeechFile = path.resolve("./public/audio/speech.mp3");
 
-    // openaiにリクエスト
+    // OpenAIにリクエスト
     const mp3 = await openai.audio.speech.create({
       model: "tts-1",
       voice: "alloy",
@@ -31,9 +24,13 @@ export async function OpenAI_TTS(
     });
 
     const buffer = Buffer.from(await mp3.arrayBuffer());
-    res.send(buffer);
+    return new NextResponse(buffer, {
+      headers: {
+        "Content-Type": "audio/mpeg",
+      },
+    });
   } catch (error) {
     console.error("Error generating speech:", error);
-    res.status(500).json({ message: "Error generating speech" });
+    return NextResponse.json({ message: "Error generating speech" }, { status: 500 });
   }
 }
