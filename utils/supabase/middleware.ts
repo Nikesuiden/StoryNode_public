1686dd8f22e670466f05aab2212ab1a0ac2b1223
Nikesuiden <taiki.nikewp@gmail.com>
2024-11-15 00:50:25 +0900

    しゅうせい

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -42,7 +42,7 @@ export async function updateSession(request: NextRequest) {
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = '/opening'
+    url.pathname = '/signin'
     return NextResponse.redirect(url)
   }
 
