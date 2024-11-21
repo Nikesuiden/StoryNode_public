624b859be8890fdcfe4eba824f7d82bee9d228a1
Nikesuiden <taiki.nikewp@gmail.com>
2024-11-21 16:35:04 +0900

    pwa 背景色変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#b8e986" />
+        <meta name="theme-color" content="#FFFFFF" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, user-scalable=no"
